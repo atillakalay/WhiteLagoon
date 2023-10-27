8e182a75a4cf55d5f18f142e4e21c1e47dad54b6
atillakalay <atilla.kalayy@gmail.com>
2023-10-27 12:36:17 +0300

    Section 4 has been complated.

--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -25,11 +25,52 @@ namespace WhiteLagoon.Web.Controllers
         {
             if (villa.Name == villa.Description)
             {
-                ModelState.AddModelError("name","The description can't exactly match the Name.");
+                ModelState.AddModelError("name", "The description can't exactly match the Name.");
             }
             if (ModelState.IsValid)
             {
                 _context.Add(villa);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Update(int id)
+        {
+            Villa? villa = _context.Villas.FirstOrDefault(x => x.Id == id);
+            if (villa == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(villa);
+        }
+        [HttpPost]
+        public IActionResult Update(Villa villa)
+        {
+            if (ModelState.IsValid && villa.Id > 0)
+            {
+                _context.Update(villa);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            Villa? villa = _context.Villas.FirstOrDefault(x => x.Id == id);
+            if (villa is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(villa);
+        }
+        [HttpPost]
+        public IActionResult Delete(Villa villa)
+        {
+            Villa? obj = _context.Villas.FirstOrDefault(x => x.Id == villa.Id);
+            if (obj is not null)
+            {
+                _context.Remove(obj);
                 _context.SaveChanges();
                 return RedirectToAction("Index");
             }
